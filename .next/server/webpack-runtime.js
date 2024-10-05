8540d3d4ee92c6576364c55235b93033fe5d25b6
Kori <kori@rogers.live>
2024-10-05 20:58:09 +0100

    added static progress bars

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e3a451d4b500b069")
+/******/ 		__webpack_require__.h = () => ("b764ee8fd5d13fdd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e41bf626d38662c0"; }
+/******/ 		__webpack_require__.h = function() { return "8e7e7e9bff9bc9d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
